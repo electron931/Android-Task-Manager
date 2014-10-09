c604dc084644c3ad75237e011328197dbdbe714f
Sergey Davydovsky <electron931@gmail.com>
2014-10-09 19:49:37 +0200

    Initial commit

---
+++ app/src/main/java/com/satansoft/android/androidtaskmanager/MainActivity.java
@@ -0,0 +1,13 @@
+package com.satansoft.android.androidtaskmanager;
+
+import android.support.v4.app.Fragment;
+
+
+public class MainActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+
+}
